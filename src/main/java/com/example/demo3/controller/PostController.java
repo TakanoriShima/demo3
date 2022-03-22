406ad746fa5e263f1956984b5a50799aaee78516
TaknaoriShima <quark2galaxy@gmail.com>
2022-03-22 18:57:24 +0900

    入力しないで投稿した時の途中まで

--- src/main/java/com/example/demo3/controller/PostController.java
+++ src/main/java/com/example/demo3/controller/PostController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,7 +48,15 @@ public class PostController {
 	 * @return 遷移先
 	 */
 	@PostMapping("/posts/create")
-	public String create(PostForm postForm, @AuthenticationPrincipal AccountUserDetails user) {
+	public String create(@Validated PostForm postForm, BindingResult bindingResult,
+			@AuthenticationPrincipal AccountUserDetails user) {
+		// バリデーションの結果、エラーがあるかどうかチェック
+		if (bindingResult.hasErrors()) {
+			// エラーがある場合は投稿登録画面を返す
+			
+			return "redirect:/posts";
+		}
+
 		Posts post = new Posts();
 		post.setName(user.getName());
 		post.setTitle(postForm.getTitle());
